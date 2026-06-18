4106ffaf323f500de6d7ad08a1c3dec38742e9dc
kanwal019 <kanwal019@gmail.com>
2026-06-19 01:56:07 +0530

    🎉Add Gradle wrapper files and initial project setup

---
+++ app/src/main/java/com/example/wearuno/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.wearuno.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun WearUnoTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
